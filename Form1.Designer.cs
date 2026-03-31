2ffb169bb71b28d05542e29a2eab1887ec46fabe
Jaqueline <jake.marie@hotmail.com>
2026-03-31 16:33:24 -0300

    feat: Cria funcionalidade de login

---
+++ Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SistemaDeLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_BoasVidas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_BoasVidas
+            // 
+            this.label_BoasVidas.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_BoasVidas.Location = new System.Drawing.Point(54, 104);
+            this.label_BoasVidas.Name = "label_BoasVidas";
+            this.label_BoasVidas.Size = new System.Drawing.Size(661, 63);
+            this.label_BoasVidas.TabIndex = 0;
+            this.label_BoasVidas.Text = "Tela Inicial";
+            this.label_BoasVidas.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_BoasVidas);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_BoasVidas;
+    }
+}
+
