a79db940051978dd9d1362a76cf2bb95f172ccbf
Jaqueline <jake.marie@hotmail.com>
2026-04-01 15:02:06 -0300

    feat: Inplementada ao sistema o cadastro do usuario e sua senha.

---
+++ CadastroUsuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeLogin
+{
+    static class CadastroUsuario
+    {
+        private static Usuario[] usuarios =
+        {
+            new Usuario(){Nome = "Jaqueline", Senha = "abc123" },
+            new Usuario(){Nome = "Danilo", Senha = "123abc" },
+            new Usuario(){Nome = "Joao Manoel", Senha = "abcd" }
+
+        };
+        private static Usuario _userLogado = null;
+
+        public static Usuario UsuarioLogado
+        {
+            get { return _userLogado; }
+           private set { _userLogado = value; }
+        }
+
+        public static bool login(string nome, string senha) 
+        {
+            foreach (Usuario usuario in usuarios) 
+            {
+                if( usuario.Nome == nome && usuario.Senha == senha)
+                {
+                    UsuarioLogado = usuario;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
